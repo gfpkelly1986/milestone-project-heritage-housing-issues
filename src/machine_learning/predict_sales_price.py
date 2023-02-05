4de2ab99ef52c53824b061e7dd997e5c13fe4732
gfpkelly1986 <gfpkelly@gmail.com>
2023-02-05 12:39:57 +0000

    Update predict sales page to show total sale value

--- src/machine_learning/predict_sales_price.py
+++ src/machine_learning/predict_sales_price.py
@@ -14,18 +14,21 @@ def predict_inherited_sale_price(X_live, pipeline):
     regressor = pipeline.named_steps['model']
     num_features = regressor.n_features_in_
    
+    
     st.write(f"\n\n\n\n")
+    st.write('---')
     st.write(f"- **These are the 4 predictions for the clients inherited houses:** \n")
-    st.write(f"- **The number of features used to train this model was: {num_features}.**")
-
     st.write(prediction)
+    st.write(f"- **The number of features used to train this model was: {num_features}.**")
+    st.write('---')
+    
     
     house_a = prediction[0].round(2)
     house_b = prediction[1].round(2)
     house_c = prediction[2].round(2)
     house_d = prediction[3].round(2)
     total = house_a + house_b + house_c + house_d
-    st.info(f"### Predicted Sale Prices:\n\n"
+    st.success(f"### Predicted Sale Prices:\n\n"
             f"House A Predicted SalePrice:  **${house_a:,}**\n\n"
             f"House B Predicted SalePrice:  **${house_b:,}**\n\n"
             f"House C Predicted SalePrice:  **${house_c:,}**\n\n"
